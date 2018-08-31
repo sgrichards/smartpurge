13b4558d7217d483d5285225847ee359399546e2
Steve Richards <steve@8balldev.uk>
2018-08-31 15:16:17 +0100

    adding travis support

--- spec/SmartPurgeFactorySpec.php
+++ spec/SmartPurgeFactorySpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\SmartPurge;
 
+use SmartPurge\SmartPurge;
 use SmartPurge\SmartPurgeFactory;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -11,5 +12,10 @@ class SmartPurgeFactorySpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(SmartPurgeFactory::class);
+    }
+
+    function it_should_build_a_smartpurge_object()
+    {
+        $this->build('username', 'sharedKey', 'shortName')->shouldBeAnInstanceOf(SmartPurge::class);
     }
 }
--- spec/SmartPurgeSpec.php
+++ spec/SmartPurgeSpec.php
@@ -2,9 +2,11 @@
 
 namespace spec\SmartPurge;
 
+use SmartPurge\Pattern\Pattern;
 use SmartPurge\SmartPurge;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use SmartPurge\Tag\Tag;
 
 class SmartPurgeSpec extends ObjectBehavior
 {
@@ -13,9 +15,19 @@ class SmartPurgeSpec extends ObjectBehavior
         $this->beConstructedWith('username', 'sharedKey', 'shortName');
     }
 
-
     function it_is_initializable()
     {
         $this->shouldHaveType(SmartPurge::class);
     }
+
+    function it_should_get_a_pattern()
+    {
+        $this->getPattern('pattern')->shouldBeAnInstanceOf(Pattern::class);
+    }
+
+    function it_should_get_a_tag()
+    {
+        $this->getTag('tag')->shouldBeAnInstanceOf(Tag::class);
+    }
+
 }
